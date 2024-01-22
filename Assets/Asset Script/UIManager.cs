ab9a71aa96ed605f6382d0a96f48e6ac6b2fcea8
Haidaradlan12 <adlannufahaidar12@gmail.com>
2024-01-22 16:39:37 +0700

    test

---
+++ Assets/Asset Script/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform MenuUtama, MenuInformasi, MenuPetunjuk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(0,0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+    }
+    public void MenuUtamabutton()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+    }
+    public void MenuInformasibutton()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+    }
+    public void MenuPetunjukbutton()
+    {
+        MenuUtama.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+        MenuPetunjuk.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+
+}
